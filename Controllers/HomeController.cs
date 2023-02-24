21268ebca6485e027126f84f80195efb980738a1
Dragus Ciprian <cipri.dragus27@gmail.com>
2023-02-24 13:53:37 +0200

    Solve mistakes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,9 +4,14 @@ namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
+        private IStoreRepository _storeRepository;
+        public HomeController(IStoreRepository storeRepository)
+        {
+            _storeRepository = storeRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_storeRepository.Products);
         }
     }
 }

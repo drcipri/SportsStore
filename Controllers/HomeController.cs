faebde17146d310c2176440c618b47bfdc860d3e
Dragus Ciprian <cipri.dragus27@gmail.com>
2023-02-26 11:05:04 +0200

    Add pagination to repository Products

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
         private IStoreRepository _storeRepository;
+        public int PageSize = 4;
         public HomeController(IStoreRepository storeRepository)
         {
             _storeRepository = storeRepository;
         }
-        public IActionResult Index()
+        public IActionResult Index(int productPage = 1)
         {
-            return View(_storeRepository.Products);
+            return View(new ProductsListViewModel
+            {
+                Products = _storeRepository.Products.OrderBy(p => p.ProductId).Skip((productPage - 1) * PageSize).Take(PageSize),
+                PagingInfo = new PagingInfo { CurrentPage = productPage, ItemsPerPage = PageSize, TotalItems = _storeRepository.Products.Count() }
+            });
         }
     }
 }

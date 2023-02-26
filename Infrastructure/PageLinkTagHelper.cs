8d3e5ed314e11141727b982d9755cd2b46cb8411
Dragus Ciprian <cipri.dragus27@gmail.com>
2023-02-26 15:35:10 +0200

    Styling the current content

--- Infrastructure/PageLinkTagHelper.cs
+++ Infrastructure/PageLinkTagHelper.cs
@@ -23,6 +23,14 @@ namespace SportsStore.Infrastructure
         public ViewContext? ViewContext { get; set; }
         public PagingInfo? PageModel { get; set; } 
         public string? PageAction { get; set; }
+
+        //this 4 are properties for div elements so we can style them
+        public bool PageClassesEnabled { get; set; }=false;
+        public string PageClass { get; set; } = string.Empty;
+        public string PageClassNormal { get;set; } = string.Empty;
+        public string PageClassSelected { get; set; } = string.Empty;
+
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if(ViewContext != null  && PageModel != null)
@@ -33,7 +41,11 @@ namespace SportsStore.Infrastructure
                 {
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new {productPage = i});
-
+                    if(PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
                     tag.InnerHtml.Append(i.ToString());
                     result.InnerHtml.AppendHtml(tag);
                 }

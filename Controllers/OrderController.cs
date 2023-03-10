50a983af96049df01fd6031c563b0994958a3639
Dragus Ciprian <cipri.dragus27@gmail.com>
2023-03-10 12:41:47 +0200

    Create Checkout Behaviour

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SportsStore.Controllers
+{
+    public class OrderController: Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
